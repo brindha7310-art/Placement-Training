8ac800f077eb9964b4d50e79dc39b54cd0fa5290
Brindha.T <brindha7310@gmail.com>
2026-07-30 14:57:45 +0530

    Create grade_using_switch.java

---
+++ DAY 3/grade_using_switch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a mark : ");
+		int mark = sc.nextInt();
+		switch(mark/10){
+		    case 10:
+		        case 9:
+		            System.out.println("Grade A");
+		            break;
+		    case 8:
+		        System.out.println("Grade B");
+		        break;
+		    case 7:
+		        System.out.println("Grade C");
+		        break;
+		    case 6:
+		        System.out.println("Grade D");
+		        break;
+		    case 5:
+		        System.out.println("Grade E");
+		        break;
+		    default:
+		        System.out.println("Fail");
+		}
+
+	}
+}
