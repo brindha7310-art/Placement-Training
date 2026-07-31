49c7fd77702efc4087fbf5059333a5b37b2936f5
Brindha.T <brindha7310@gmail.com>
2026-07-31 12:10:11 +0530

    Create Average.java

---
+++ DAY 2/Average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        double avg = (a + b + c) / 3.0;
+        System.out.println("Average = " + avg);
+    }
+}
